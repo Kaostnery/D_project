ca6f63bb8f7696347a0c99d279a9e5a211fd5cb5
Loxirayax <121885240+Kaostnery@users.noreply.github.com>
2026-05-31 12:18:41 +0200

    Update plateforme.js

--- plateforme.js
+++ plateforme.js
@@ -70,7 +70,7 @@ function render() {
    ========================= */
 
 function unlock() {
-    const code = $("code").value.trim().toUpperCase();
+    const code = $("code").value.trim();
     const statusEl = $("status");
 
     if (!ARCHIVES[code]) {
